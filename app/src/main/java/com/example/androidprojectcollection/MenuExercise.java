f43fb03a2f4150186a924e696a68152aae9a1a8f
aizernere <aizernerevangelista@gmail.com>
2024-04-24 14:24:43 +0800

    Menu Finished

--- app/src/main/java/com/example/androidprojectcollection/MenuExercise.java
+++ app/src/main/java/com/example/androidprojectcollection/MenuExercise.java
@@ -2,11 +2,18 @@ package com.example.androidprojectcollection;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -17,6 +24,12 @@ public class MenuExercise extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_exercise);
         btnChanger = findViewById(R.id.btnTransformingButton);
+        btnChanger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MenuExercise.this, "Your button is working.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -28,13 +41,46 @@ public class MenuExercise extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected (@NonNull MenuItem item){
-//        if(item.getItemId() == R.id.mItemChange){
-//            Toast.makeText(this, "Edit Object Item is clicked", Toast.LENGTH_SHORT).show();
-//        }else if(item.getItemId() == R.id.mItemReset){
-//            Toast.makeText(this, "Reset Object Item is clicked", Toast.LENGTH_SHORT).show();
-//        }else if(item.getItemId() == R.id.mItemExit){
-//            finish();
-//        }
+        switch (item.getItemId()){
+            case R.id.mItemChange:
+                Toast.makeText(this, "Edit Object Item is clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mItemTextSize:
+                Toast.makeText(this, "Text Size is changed.", Toast.LENGTH_SHORT).show();
+                btnChanger.setTextSize(7);
+                break;
+            case R.id.mItemTextColor:
+                Toast.makeText(this, "Text Color is changed.", Toast.LENGTH_SHORT).show();
+                btnChanger.setTextColor(Color.parseColor("#2e5467"));
+                break;
+            case R.id.mItemDisable:
+                Toast.makeText(this, "Button is now disabled", Toast.LENGTH_SHORT).show();
+                btnChanger.setEnabled(false);
+                break;
+            case R.id.mItemSquare:
+                Toast.makeText(this, "Button is now squared.", Toast.LENGTH_SHORT).show();
+                btnChanger.setBackground(getResources().getDrawable(R.drawable.squarebutton));
+                break;
+            case R.id.mItemBlue:
+                Toast.makeText(this, "Button is now Light Blue.", Toast.LENGTH_SHORT).show();
+                btnChanger.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.lightBlue)));
+                break;
+            case R.id.mItemReset:
+                Toast.makeText(this, "Reset Object Item is clicked", Toast.LENGTH_SHORT).show();
+                btnChanger.setTextSize(20);
+                btnChanger.setTextColor(Color.parseColor("#FFFFFF"));
+                btnChanger.setEnabled(true);
+                btnChanger.setBackground(getResources().getDrawable(R.drawable.roundbutton));
+                btnChanger.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.purple_500)));
+                break;
+            case R.id.mItemExit:
+                Toast.makeText(this, "Exiting...", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+            default:
+                break;
+        }
+
         return true;
     }
 }
